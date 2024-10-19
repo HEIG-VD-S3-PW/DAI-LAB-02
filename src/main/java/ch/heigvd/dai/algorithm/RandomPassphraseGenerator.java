c8489a84ab7a7f2be71f1407ad575bdd48c15f3d
mathieuemery <mathieu.emery@heig-vd.ch>
2024-10-19 18:11:31 +0200

    Correction du bug avec la passphrase

--- src/main/java/ch/heigvd/dai/algorithm/RandomPassphraseGenerator.java
+++ src/main/java/ch/heigvd/dai/algorithm/RandomPassphraseGenerator.java
@@ -11,14 +11,14 @@ public class RandomPassphraseGenerator {
      */
     public static String  generator(){
         Random r = new Random();
-        final int firstChar = 21; // '!'
+        final int firstChar = 33; // '!'
         final int lastChar = 126; // '~'
         final int passphraseLenght = 10;
 
         String passphrase = "";
 
         for(int i = 0; i < passphraseLenght; ++i){
-            char a = '!';
+            char a = firstChar;
             a += (char) r.nextInt(lastChar-firstChar);
             passphrase += a;
         }
